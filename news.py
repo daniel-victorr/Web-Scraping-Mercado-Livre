cae4b14eaa3542ccad512f3429590405d87a067c
daniel.costa <daniel.costa@tron.com.br>
2023-05-24 14:22:11 -0300

    add not found

--- news.py
+++ news.py
@@ -16,23 +16,28 @@ response = requests.get(url)
 site = BeautifulSoup(response.content, 'html.parser')
 sleep(1)
 
-produtos = site.findAll('div', attrs={'class': 'ui-search-result__wrapper shops__result-wrapper'})
-for produto in produtos:
-    link = produto.find('a', attrs={'class': 'ui-search-item__group__element shops__items-group-details ui-search-link'})
-    img = produto.get('img')
-    descricao = produto.find('h2', attrs={'class': 'ui-search-item__title shops__item-title'})
-    frete = produto.find('p', attrs={'class': 'ui-search-item__shipping ui-search-item__shipping--free shops__item-shipping-free'})
-    preco_atual = produto.find('div', attrs={'class': 'ui-search-price__second-line shops__price-second-line'}).find('span', attrs={'class': 'price-tag-text-sr-only'})
-    preco_antes = produto.find('span', attrs={'class': 'price-tag-text-sr-only'})
-    
-    if(frete):
-        dados.append([descricao.text, preco_antes.text, preco_atual.text, link.get('href'), frete.text])
-    else:
-        dados.append([descricao.text, preco_antes.text, preco_atual.text, link.get('href'), 'Frete acombinar'])
+not_found = site.find('h3', attrs={'class': 'ui-search-rescue__title'})
 
-news = pd.DataFrame(dados, columns=['Descrição', 'Preço antes', 'Preço atual', 'Link', 'Frete'])  
-news.to_csv('Scraping_Mercado_Livre.csv', index=False)
-    
+if not not_found:
+    produtos = site.findAll('div', attrs={'class': 'ui-search-result__wrapper shops__result-wrapper'})
+    for produto in produtos:
+        link = produto.find('a', attrs={'class': 'ui-search-item__group__element shops__items-group-details ui-search-link'})
+        img = produto.get('img')
+        descricao = produto.find('h2', attrs={'class': 'ui-search-item__title shops__item-title'})
+        frete = produto.find('p', attrs={'class': 'ui-search-item__shipping ui-search-item__shipping--free shops__item-shipping-free'})
+        preco_atual = produto.find('div', attrs={'class': 'ui-search-price__second-line shops__price-second-line'}).find('span', attrs={'class': 'price-tag-text-sr-only'})
+        preco_antes = produto.find('span', attrs={'class': 'price-tag-text-sr-only'})
+        
+        if(frete):
+            dados.append([descricao.text, preco_antes.text, preco_atual.text, link.get('href'), frete.text])
+        else:
+            dados.append([descricao.text, preco_antes.text, preco_atual.text, link.get('href'), 'Frete acombinar'])
+        
+        news = pd.DataFrame(dados, columns=['Descrição', 'Preço antes', 'Preço atual', 'Link', 'Frete'])  
+        news.to_csv('Scraping_Mercado_Livre.csv', index=False)
+else:
+    print(not_found.text)
+
 
 
 
